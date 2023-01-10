56b6b9919f579be79d5aaf3bb379b0131b7806be
Justin Hui <jhui@lcs.on.ca>
2023-01-10 11:34:44 -0500

    Created dog and book structure

--- Thread1Recap.playground/Contents.swift
+++ Thread1Recap.playground/Contents.swift
@@ -1,3 +1,31 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+struct Dog {
+    
+    //MARK: Stored Properties
+    let breed: String
+    let color: String
+    var heightInMetres: Double
+    let length: Double
+    let height: Double
+    let mass: Double
+    
+    //MARK: Computed Properties
+    var ratio: Double {
+        return length / height
+    }
+}
+
+
+struct book {
+    let title: String
+    let author: String
+    let Color: String
+    let length: Double
+    let width: Double
+    let height: Double
+    
+    var volumeOfBook: Double {
+        return width * length * height
+    }
+}
